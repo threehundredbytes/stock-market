175a51d3cde26f0e3e342fdfee749948c712e52a
Anton Kosarev <antonkosarev.dev@gmail.com>
2022-08-25 00:24:35 +0200

    Refactoring - migrating to Redis, removing testing

--- stock-price-service/src/main/java/ru/dreadblade/stockmarket/stockpriceservice/event/handler/StockCreatedIntegrationEventHandler.java
+++ stock-price-service/src/main/java/ru/dreadblade/stockmarket/stockpriceservice/event/handler/StockCreatedIntegrationEventHandler.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import ru.dreadblade.stockmarket.stockpriceservice.domain.Stock;
+import ru.dreadblade.stockmarket.stockpriceservice.model.StockPrice;
 import ru.dreadblade.stockmarket.stockpriceservice.event.StockCreatedIntegrationEvent;
 import ru.dreadblade.stockmarket.stockpriceservice.repository.StockRepository;
 
@@ -20,10 +20,9 @@ public class StockCreatedIntegrationEventHandler implements IntegrationEventHand
         log.trace("Handling integration event: {} ({}): {}\n", integrationEvent.getId().toString(),
                 integrationEvent.getClass().getSimpleName(), integrationEvent.getCreatedAt().toString());
 
-        Stock stock = Stock.builder()
-                .id(integrationEvent.getStockId())
+        StockPrice stock = StockPrice.builder()
+                .stockId(integrationEvent.getStockId())
                 .price(integrationEvent.getStockPrice())
-                .createdAt(integrationEvent.getStockCreatedAt())
                 .build();
 
         stockRepository.save(stock);
--- stock-price-service/src/main/java/ru/dreadblade/stockmarket/stockpriceservice/service/StockPriceService.java
+++ stock-price-service/src/main/java/ru/dreadblade/stockmarket/stockpriceservice/service/StockPriceService.java
@@ -10,6 +10,7 @@ import ru.dreadblade.stockmarket.stockpriceservice.event.bus.EventBus;
 import ru.dreadblade.stockmarket.stockpriceservice.repository.StockRepository;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.Instant;
 
 @Service
@@ -24,43 +25,44 @@ public class StockPriceService {
         log.trace("Changing stock prices");
 
         stockRepository.findAll().stream()
+                .filter(s -> RandomUtils.nextInt(0, 100) <= 42)
                 .peek(stock -> {
-                    if (RandomUtils.nextInt(0, 100) <= 42) {
-                        BigDecimal priceFactor = new BigDecimal("0.001");
-                        BigDecimal randomPriceFactor = new BigDecimal(RandomUtils.nextInt(1, 3));
+                    BigDecimal priceFactor = new BigDecimal("0.001");
+                    BigDecimal randomPriceFactor = new BigDecimal(RandomUtils.nextInt(2, 3));
 
-                        int randomInt = RandomUtils.nextInt(0, 100);
+                    int randomInt = RandomUtils.nextInt(0, 100);
 
-                        if (randomInt == 99) {
-                            randomPriceFactor = new BigDecimal(RandomUtils.nextInt(9, 11));
-                        } else if (randomInt == 98) {
-                            randomPriceFactor = new BigDecimal(RandomUtils.nextInt(7, 9));
-                        } else if (randomInt == 97) {
-                            randomPriceFactor = new BigDecimal(RandomUtils.nextInt(5, 7));
-                        } else if (randomInt >= 95) {
-                            randomPriceFactor = new BigDecimal(RandomUtils.nextInt(3, 5));
-                        }
-
-                        BigDecimal priceAbsoluteChange = stock.getPrice().multiply(priceFactor.multiply(randomPriceFactor));
-
-                        boolean isGrowth = RandomUtils.nextInt(0, 100) >= 42;
-
-                        if (isGrowth) {
-                            stock.setPrice(stock.getPrice().add(priceAbsoluteChange));
-                        } else {
-                            stock.setPrice(stock.getPrice().subtract(priceAbsoluteChange));
-                        }
+                    if (randomInt == 99) {
+                        randomPriceFactor = new BigDecimal(RandomUtils.nextInt(9, 11));
+                    } else if (randomInt == 98) {
+                        randomPriceFactor = new BigDecimal(RandomUtils.nextInt(7, 9));
+                    } else if (randomInt == 97) {
+                        randomPriceFactor = new BigDecimal(RandomUtils.nextInt(5, 7));
+                    } else if (randomInt >= 95) {
+                        randomPriceFactor = new BigDecimal(RandomUtils.nextInt(3, 5));
                     }
 
-                    stock.setCreatedAt(Instant.now());
+                    BigDecimal priceAbsoluteChange = stock.getPrice().multiply(priceFactor.multiply(randomPriceFactor));
+
+                    boolean isGrowth = RandomUtils.nextInt(0, 100) <= 49;
+
+                    BigDecimal price = stock.getPrice();
+
+                    if (isGrowth) {
+                        price = price.add(priceAbsoluteChange);
+                    } else {
+                        price = price.subtract(priceAbsoluteChange);
+                    }
+
+                    stock.setPrice(price.setScale(2, RoundingMode.HALF_EVEN));
                 })
                 .forEach(stock -> {
                     stockRepository.save(stock);
 
                     StockPriceChangeIntegrationEvent event = StockPriceChangeIntegrationEvent.builder()
-                            .stockId(stock.getId())
+                            .stockId(stock.getStockId())
                             .newPrice(stock.getPrice())
-                            .changedAt(stock.getCreatedAt())
+                            .changedAt(Instant.now())
                             .build();
 
                     eventBus.publish("stock-price-changes", event);
