e51baa3980c9e8e9745855c7834152b2176a890c
Anton Kosarev <antonkosarev.dev@gmail.com>
2022-09-09 22:16:56 +0200

    Initializing discovery-service
    Created a parent Maven project for Spring infrastructure services

--- services/stock-price-history-service/src/main/java/ru/dreadblade/stockmarket/stockpricehistoryservice/StockPriceHistoryServiceApplication.java
+++ services/stock-price-history-service/src/main/java/ru/dreadblade/stockmarket/stockpricehistoryservice/StockPriceHistoryServiceApplication.java
@@ -2,8 +2,10 @@ package ru.dreadblade.stockmarket.stockpricehistoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StockPriceHistoryServiceApplication {
 
 	public static void main(String[] args) {
--- services/stock-price-service/src/main/java/ru/dreadblade/stockmarket/stockpriceservice/StockPriceServiceApplication.java
+++ services/stock-price-service/src/main/java/ru/dreadblade/stockmarket/stockpriceservice/StockPriceServiceApplication.java
@@ -2,8 +2,10 @@ package ru.dreadblade.stockmarket.stockpriceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StockPriceServiceApplication {
 
 	public static void main(String[] args) {
